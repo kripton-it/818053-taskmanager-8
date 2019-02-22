e8a78564c37f475cfde8ca73af40f60096f56826
Mikhail Shardin <kriptonit1988@gmail.com>
2019-02-22 17:45:38 +0500

    Разбил проект на модули

---
+++ src/get-filter.js
@@ -0,0 +1,19 @@
+export default ({caption, amount, isDisabled = false, isChecked = false}) => {
+  const checkedAttribute = isChecked ? ` checked` : ``;
+  const disabledAttribute = isDisabled ? ` disabled` : ``;
+  const idAttribute = `filter__${caption.toLowerCase()}`;
+  const labelClassAttribute = `${idAttribute}-count`;
+  return `
+  <input
+    type="radio"
+    id="${idAttribute}"
+    class="filter__input visually-hidden"
+    name="filter"
+    ${checkedAttribute}
+    ${disabledAttribute}
+  />
+  <label for="${idAttribute}" class="filter__label">
+  ${caption.toUpperCase()} <span class="${labelClassAttribute}">${amount}</span></label>
+`;
+};
+
